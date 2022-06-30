719c3c21c9a468ba45d13c25a784d40962b1c6c0
yupjun <yaup22c@naver.com>
2022-06-30 17:54:52 +0900

    add rectangle one

--- Sierpinski_triangle/triangle.py
+++ Sierpinski_triangle/triangle.py
@@ -13,21 +13,45 @@ def uniform_triangle(u, v):
 
 
 # Sierpinski triangle
-class Triangle(Scene):
+class Sierpinski_triangle(Scene):
     def construct(self):
         colorList = [RED, GREEN, BLUE, YELLOW]
         triangle_point = [np.array([0,3,0]),np.array([-3,-3,0]),np.array([3,-3 ,0])]
         for points in triangle_point:
             point = Point(location=points,color=np.random.choice(colorList))
             self.add(point)
-        init_point = uniform_triangle(
-            triangle_point[1] - triangle_point[0],
-            triangle_point[2] - triangle_point[0],
-                    )
+        init_point = np.array([random.uniform(-3, 3),random.uniform(-3, 3),random.uniform(-3, 3)])
         first_point = Point(location=init_point,color=np.random.choice(colorList))
         self.add(first_point)
         for nums in range(5000):
             obj = random.choice(triangle_point)
+            init_point += obj
+            init_point =init_point/2
+            if nums%2:
+                picked_color = RED
+            else:
+                picked_color = BLUE
+            point =  Point(location=init_point,color=picked_color)
+            self.add(point)
+
+
+
+
+
+# Sierpinski rectangle
+class Sierpinski_rectangle(Scene):
+    def construct(self):
+        colorList = [RED, GREEN, BLUE, YELLOW]
+        rectangle_point = [np.array([3,3,0]),np.array([-3,-3,0]),np.array([3,-3 ,0]),np.array([-3,3 ,0])]
+        for points in rectangle_point:
+            point = Point(location=points,color=np.random.choice(colorList))
+            self.add(point)
+        init_point = np.array([random.uniform(-3, 3),random.uniform(-3, 3),random.uniform(-3, 3)])
+        first_point = Point(location=init_point,color=np.random.choice(colorList))
+        self.add(first_point)
+        for nums in range(1000):
+            # obj = rectangle_point[nums%4
+            obj =  random.choice(rectangle_point)
             init_point += obj
             init_point =init_point/2
             if nums%2:
